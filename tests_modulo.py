85d49712063a12a0fe38d61c5544c2b940ac2d05
dublinsubway <61555192+dublinsubway@users.noreply.github.com>
2022-09-04 17:43:27 +0100

    Add typeerror tests

--- tests_modulo.py
+++ tests_modulo.py
@@ -9,3 +9,11 @@ name = str("ado-pipeline")
 ])
 def test_correct_data(numbers):
     assert modulo(numbers[0], numbers[1]) == 0
+
+@pytest.mark.parametrize("data", [
+[10, 'a'],
+['b', 'c']
+])
+def test_incorrect_data(data):
+    with pytest.raises(TypeError):
+        assert modulo(data[0], data[1]) == 0
